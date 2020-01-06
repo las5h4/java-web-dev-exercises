e4ab2885d4f8854cacffbc1ab9e85c78c3906f0e
las5h4 <loganasmalley@gmail.com>
2020-01-06 15:38:01 -0600

    finished exercise 4

---
+++ src/exercises/Alice.java
@@ -0,0 +1,14 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Alice {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String sentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+        System.out.println("Enter a term to search for:");
+        String term = input.nextLine();
+        Boolean answer = sentence.toLowerCase().contains(term.toLowerCase());
+        System.out.println(answer);
+    }
+}
